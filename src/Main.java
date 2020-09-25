456c6ae8bfae9c63740c0aed5d9952bc61191df3
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-09-25 21:00:42 +0200

    Guest list

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
+
+// 2:07:45
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        boolean shouldConitnue = true;
 
-        System.out.println("jakaś zmiana do Gita");
+        Party party = new Party();
 
+        while (shouldConitnue) {
+            System.out.println("Wybierz opcję");
+            System.out.println("1. Wyświetl gości");
+            System.out.println("2. Dodaj gościa");
+            System.out.println("3. Wyświetl potrawy");
+            System.out.println("4. Znajdź po numerze telefonu");
+            System.out.println("5. Wyjście");
+
+            int userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+                case 1:
+                    party.displayGuests();
+                    break;
+                case 2:
+                    party.addGuest();
+                    break;
+                case 3:
+                    System.out.println("wybrano 3");
+                    break;
+                case 4:
+                    System.out.println("wybrano 4");
+                    break;
+                case 5:
+                    shouldConitnue = false;
+                    break;
+            }
+        }
     }
 }
